37534b172f7b7643066ec940429643a6006802dc
sunchengcheng <sun@chengcheng.info>
2015-03-05 15:16:55 +0800

    准备写models.py

--- web/models.py
+++ web/models.py
@@ -1,3 +1,14 @@
+# encoding: utf-8
 from django.db import models
 
 # Create your models here.
+
+
+class CATEGORY(models.Model):
+    name = models.CharField(u'分类名', max_length=32)
+
+
+class TORRENT(models.Model):
+    name = models.CharField(u'名称', max_length=128)
+    content = models.BinaryField(u'文件内容')
+    category = models.ForeignKey(CATEGORY)
